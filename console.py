1c083d044f45573d7863ae8d2ff801d852284c06
Felipe Prado <jfpc11@misena.edu.co>
2021-06-26 17:08:21 -0500

    uptaing yet

--- console.py
+++ console.py
@@ -6,6 +6,8 @@ cmd that will be used for creating the command line
 imterpreter
 """
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -15,7 +17,7 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
     file = None
 
-    def do_quit(self, arg):
+    def do_quit(self, args):
         """Command to exit the prompt directly
         """
         return True
@@ -28,7 +30,64 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """When a empty line is passed, does not do
-        anything
+        anything""""""
+        """
+        pass
+
+    def do_create(self, args):
+        """Creates a new instance of BaseModel, saves it, and print his id.
+            If the class name is missing it raise the following error:
+                ** class name missing **
+            If the class name doesn’t exist it raise the following error:
+                ** class doesn't exist **
+        """
+        if not args:
+            print("** class name missing **")
+        elif args != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            obj = BaseModel()
+            obj.save()
+            print(obj.id)
+
+    def do_show(self, *args):
+        """Prints the string representation of an instance based on the class name and id
+            If the class name is missing it raise the following error:
+                ** class name missing **
+            If the class name doesn’t exist it raise the following error:
+                ** class doesn't exist **
+            If the id is missing it raise the following error:
+                ** instance id missing **
+            If the instance of the class name doesn’t exist for the id it raise the following error:
+                ** no instance found **
+        """
+        created_objs = storage.all()
+        if not args[0]:
+            print("** class name missing **")
+            return
+        elif not args[1]:
+            print("** instance id missing **")
+            return
+        elif args != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        elif args and args[1]:
+            new_key = args + "." + args[1]
+        if any(new_key == keys for keys in created_objs.keys()):
+            print(new_key.to_dict())
+
+    def do_destroy(self):
+        """
+        """
+        pass
+
+    def do_all(self):
+        """
+        """
+        pass
+
+    def do_update(self):
+        """
         """
         pass
 
