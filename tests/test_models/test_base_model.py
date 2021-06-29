562ddfc004db8217cfb1310ee86764981b12d4e1
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-06-29 15:15:47 -0500

    adding tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,8 +8,17 @@ from models.base_model import BaseModel
 class TestBaseModel(unittest.TestCase):
     """Class for testing the object base model"""
 
+    def test_is_instance(self):
+        """check if the rigth object is being created"""
+        model = BaseModel()
+        self.assertIsInstance(model, BaseModel)
+        # up_dated
+        # created
+        # class
+        # id
+
     def test_setattributes(self):
-        """check if the attributes are being created"""
+        """check if the attributes are being created propertly"""
         a1 = BaseModel()
         a1.name = "Holberton"
         self.assertEqual(a1.name, "Holberton")
@@ -17,11 +26,28 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(a1.my_number, 89)
         a1.my_number = -1
         self.assertEqual(a1.my_number, -1)
+        a1.my_number = 0.1
+        self.assertEqual(a1.my_number, 0.1)
         a1.name = ""
         self.assertEqual(a1.name, "")
 
     def test_dict(self):
-        pass
+        model = BaseModel()
+        dict_proper = model.to_dict()
+        id_ = model.id
+        time_created = model.created_at.isoformat()
+        time_updated = model.updated_at.isoformat()
+        dict_expected = {'__class__': 'BaseModel', 'id': id_,
+                         'updated_at': time_updated, 
+                         'created_at': time_created}
+        self.assertDictEqual(dict_proper, dict_expected)
+
+    def test_string_representation(self):
+        """test the string represtation of the
+        class BaseModel is correct"""
+        model = BaseModel()
+        string = "[BaseModel] ({}) {}".format(model.id, model.__dict__)
+        self.assertEqual(model.__str__(), string)
 
 
 if __name__ == '__main__':
